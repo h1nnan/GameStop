53c3ae47e5c9954361455a682367805782ef9061
Hannan Butt <buttha@s164537.eschool.de>
2025-02-10 09:34:55 +0100

    Initial commit

---
+++ src/Order.java
@@ -0,0 +1,24 @@
+public class Confirm {
+    private boolean confirm;
+    private int indr;
+
+    Bestellung(Game pWare){
+
+    }
+
+    public boolean getConfirm() {
+        return confirm;
+    }
+
+    public void setBesteatigt(boolean confirm) {
+        this.confirm = confirm;
+    }
+
+    public int getIndr() {
+        return indr;
+    }
+
+    public void setIndr(int indr) {
+        this.indr = indr;
+    }
+}
